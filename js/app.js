fcf257c921b990f584c1f005b9f2440cdf28ee63
Manuel Lopez <lopez6667@hotmail.com>
2022-04-25 21:03:20 -0500

    final10

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if(navigator.serviceWorker) {
-    navigator.serviceWorker.register("Bakery-new/sw.js");
+    navigator.serviceWorker.register("/Bakery-new/sw.js");
     console.log("Si funciono, ya la armaste");
 } else {
     console.log("No funciona, rindete :)")
